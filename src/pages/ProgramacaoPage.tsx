de30a7a25d0a519121d78cc20192c60886fb4231
chicowilliam <viniciuswilliam91@gmail.com>
2026-07-14 18:29:19 -0300

    Add dedicated Programacao page.

---
+++ src/pages/ProgramacaoPage.tsx
@@ -0,0 +1,9 @@
+import { Programacao } from "@/components/Programacao";
+
+export default function ProgramacaoPage() {
+  return (
+    <main className="min-h-dvh" data-page="programacao">
+      <Programacao />
+    </main>
+  );
+}
