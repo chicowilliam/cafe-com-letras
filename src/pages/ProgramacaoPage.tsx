22ad0909d307a1d33c5f3fb13761a608b98fa4f6
chicowilliam <viniciuswilliam91@gmail.com>
2026-07-17 18:41:02 -0300

    ﻿Add section-stack to Programacao page.

--- src/pages/ProgramacaoPage.tsx
+++ src/pages/ProgramacaoPage.tsx
@@ -2,7 +2,7 @@ import { Programacao } from "@/components/Programacao";
 
 export default function ProgramacaoPage() {
   return (
-    <main className="min-h-dvh" data-page="programacao">
+    <main className="section-stack min-h-dvh" data-page="programacao">
       <Programacao />
     </main>
   );
